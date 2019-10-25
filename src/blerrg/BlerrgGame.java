1cf259fe0c63ef45837fc7c5aea2bc434a3edc01
Jonathan Davis <jonathan.r.davis@wsu.edu>
2019-10-24 21:27:55 -0700

    Create initial working game window.

---
+++ src/blerrg/BlerrgGame.java
@@ -0,0 +1,40 @@
+package blerrg;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class BlerrgGame extends StateBasedGame {
+	
+	public final static int STARTSTATE = 0;
+	
+	public final int ScreenWidth;
+	public final int ScreenHeight;
+
+	public BlerrgGame(String title, int width, int height) throws SlickException {
+		super(title);
+		ScreenWidth = width;
+		ScreenHeight = height;
+	}
+	
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		addState(new StartState());
+	}
+
+	public static void main(String[] args) {
+		AppGameContainer app;
+		try {
+			app = new AppGameContainer(new BlerrgGame(":: Blerrg ::", 1280, 720));
+			app.setDisplayMode(1280, 720, false);
+			app.setShowFPS(true);
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+
+}
