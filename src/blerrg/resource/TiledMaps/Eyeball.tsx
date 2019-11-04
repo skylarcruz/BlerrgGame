3570759e2ddf02e66e61c06661cb6c96ccddd80b
Blaine Davison Kelley <blaine.kelley@wsu.edu>
2019-11-04 13:43:31 -0800

    Added initial tile sets, stand-in sprite and tiled map in separate directory in resources

---
+++ src/blerrg/resource/TiledMaps/Eyeball.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Eyeball" tilewidth="32" tileheight="38" tilecount="12" columns="3">
+ <image source="../Project 1/Cat Resources/LPC Base Assets/sprites/monsters/eyeball.png" width="96" height="152"/>
+</tileset>
